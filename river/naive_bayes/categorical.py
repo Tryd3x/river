8e1da2f42b734283cf854436c66355e2021845bb
Trydex <hyderrezatelegraphy@gmail.com>
2023-11-21 22:01:29 -0600

    implemented 3 methods for CategoricalNB

--- river/naive_bayes/categorical.py
+++ river/naive_bayes/categorical.py
@@ -1,11 +1,10 @@
 from __future__ import annotations
 
-import collections
-import math
+from collections import defaultdict
 
 import numpy as np
 import pandas as pd
-from scipy import sparse
+# from scipy import sparse
 
 from . import base
 
@@ -17,29 +16,40 @@ class CategoricalNB(base.BaseNB):
 
     def __init__(self, alpha = 1.0):
         self.alpha = alpha
-        self.class_counts = collections.Counter()
-        self.feature_counts = collections.defaultdict(collections.Counter)
+        self.class_counts = defaultdict(int)
+        self.feature_counts = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
 
-    def learn_one(self, x, y):
-        pass
-        # self.class_counts.update((y,))
-        # for i, t in x.items():
-            # feature_counts[i][t].update(y)
+    def learn_one(self, x: dict, y):
+        y = str(y).lower()
+        # Update class count
+        self.class_counts[y] +=1
 
-    def p_feature_given_class(self, feature, label):
-        num = self.feature_counts.get(feature, {}).get(label, 0.0) + self.alpha
-        den = self.class_counts[label] + self.alpha * len(self.feature_counts)
+        # Update feature count given class in model
+        for i,t in x.items():
+            self.feature_counts[i][t][y]+=1
+
+        return self
+
+    def p_feature_given_class(self, feature, category, label):
+        num = self.feature_counts.get(feature, {}).get(category, {0.0}).get(label,0) + self.alpha
+        den = self.class_counts[label] + self.alpha * len(self.feature_counts.get(feature,{}).keys())
 
         return num/den
     
-    def p_class(self,):
-        pass
+    def p_class(self,label):
+        return self.class_counts[label]/sum(self.class_counts.values())
 
-    def joint_log_likelihood(self):
-        pass
+    def joint_log_likelihood(self,x: dict):
+        return {y : np.log(self.p_class(y))
+                + sum([
+                    np.log(self.p_feature_given_class(
+                        feature=i,
+                        category=t,
+                        label=y)) for i,t in x.items()])
+                for y in self.class_counts.keys()}
 
     def learn_many(self):
-        pass
+        return self
 
     def _feature_log_prob(self):
         pass
