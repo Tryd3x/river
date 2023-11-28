94559eb144c81b62b6faf6a9a0bda93746d785d7
Trydex <hyderrezatelegraphy@gmail.com>
2023-11-28 15:25:02 -0600

    Added learn_many() implementation

--- river/naive_bayes/categorical.py
+++ river/naive_bayes/categorical.py
@@ -9,7 +9,7 @@ from scipy import sparse
 from . import base
 
 __all__ = ["CategoricalNB"]
-
+ 
 class CategoricalNB(base.BaseNB):
     """ Naive Bayes classifier for categorical models.
     """
@@ -17,7 +17,8 @@ class CategoricalNB(base.BaseNB):
     def __init__(self, alpha = 1.0):
         self.alpha = alpha
         self.class_counts = defaultdict(int)
-        self.feature_counts = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
+        self.feature_counts = defaultdict(lambda: defaultdict(int))
+        self.category_counts = defaultdict(set)
 
     def learn_one(self, x: dict, y):
         y = str(y).lower()
@@ -26,13 +27,17 @@ class CategoricalNB(base.BaseNB):
 
         # Update feature count given class in model
         for i,t in x.items():
-            self.feature_counts[i][t][y]+=1
+            self.category_counts[i].add(t)
+            self.feature_counts[f"{i}_{t}"][y]+=1
 
         return self
 
     def p_feature_given_class(self, feature, category, label):
-        num = self.feature_counts.get(feature, {}).get(category, {0.0}).get(label,0) + self.alpha
-        den = self.class_counts[label] + self.alpha * len(self.feature_counts.get(feature,{}).keys())
+        # under work
+
+        num = self.feature_counts.get(f"{feature}_{category}", {}).get(label,0) + self.alpha
+        print(num)
+        den = self.class_counts[label] + self.alpha * len(self.category_counts[feature])
 
         return num/den
     
@@ -49,19 +54,31 @@ class CategoricalNB(base.BaseNB):
                 for y in self.class_counts.keys()}
 
     def learn_many(self, X: pd.DataFrame, y: pd.Series):
-        y = base.one_hot_encode(y)
-        classes = str(y.columns).lower()
-        y = sparse.csc_matrix(y).T
 
+        X = self._encodeX(X)
+        y = base.one_hot_encode(y)
+        features, classes =  X.columns, [str(c).lower() for c in y.columns]
+
+        y = sparse.csc_matrix(y).T
         # update class counts
         for label, count in zip(classes, y.sum(axis=1)):
             self.class_counts[label] += count.item()
-
-        # update feature count
-
         
+        # update feature counts
+        fc = y @ X
 
-        return self
+        for r,c in zip(*fc.nonzero()):
+            self.feature_counts[features[c]][classes[r]] += fc[r,c]
+
 
     def joint_log_likelihood_many(self): 
         pass
+
+    def _encodeX(self, df: pd.DataFrame):
+        feature_subsets = []
+        for col in df.columns:
+            temp = base.one_hot_encode(df[col])
+            temp.columns = [f"{col}_{temp_col}" for temp_col in temp.columns]
+            feature_subsets.append(temp)
+        
+        return pd.concat(feature_subsets,axis=1)
